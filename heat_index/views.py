de3089a83f58e397621f47828f57fa47c0036361
Jose Manuel Gama Estrada <josegamaes@gmail.com>
2018-08-15 13:09:49 -0500

    html page

--- heat_index/views.py
+++ heat_index/views.py
@@ -10,18 +10,24 @@ import firebase_admin
 class IndexView(View):
     # Load Firebase data
     cred = credentials.Certificate(
-        '/home/jmgamest/Documents/Programming/IoT-Heat-Index/heat_index/ServiceAccountKey.json')
+        '/home/jmgamest/Documents/Programming/IoT-Heat-Index/ServiceAccountKey.json')
     default_app = firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://iot-heat-index.firebaseio.com/'
     })
 
     def get(self, request):
-        data = db.reference().get()
-        # data = {'temperature': 30, 'humidity':50}
+        # data = db.reference().get()
+        
+        # context = {
+        #     'temperature': data['temperature'],
+        #     'humidity': data['humidity']
+        # }
         
         context = {
-            'temperature': data['temperature'],
-            'humidity': data['humidity']
+            'temperature': 30,
+            'humidity': 55,
+            'heat_index': 12,
+            'control': 1
         }
 
         return render(request, 'heat_index/index.html', context)
